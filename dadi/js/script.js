0291a14ad5ea695eb17736eddfd97c55d0c1d722
TrvMarco <trevisanello.marco@gmail.com>
2022-05-03 18:12:00 +0200

    fixato stile css e html

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -2,7 +2,7 @@
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
 // creare una variabile per il giocatore e una per il computer
-let userNumber = Number(prompt("inserisci un numero da 1 a 6"));
+let userNumber = Math.floor(Math.random() * 6) + 1;
 let computerNumber = Math.floor(Math.random() * 6) + 1;
 
 console.log(userNumber);
@@ -20,12 +20,12 @@ const result = document.querySelector(".result")
 
 // creare la condizione che permette di vincere a chi ha fatto il numero pìu alto 
 if( userNumber > computerNumber){
-    result.innerHTML = "HAI VINTO!"
+    result.innerHTML = "L'utente è il vincitore"
     // alert(`"Hai vinto!"${userNumber}"vs"${computerNumber}`)
 }else if(userNumber === computerNumber){
     result.innerHTML = "Pareggio!"
     // alert(`"Pareggio!!"${userNumber}"vs"${computerNumber}`)
 }else{
-    result.innerHTML = "HAI PERSO!"
+    result.innerHTML = "Il computer ha vinto"
     // alert(`"Hai perso!"${computerNumber}"vs"${userNumber}`)
 }
