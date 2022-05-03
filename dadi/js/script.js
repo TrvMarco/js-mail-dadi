2cdf18c4a2ca25efb6b41b8ef7dc824431430639
TrvMarco <trevisanello.marco@gmail.com>
2022-05-03 18:24:22 +0200

    aggiunto bottone e script per far iniziare e ripetere il gioco

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,31 +1,29 @@
-// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
-// Stabilire il vincitore, in base a chi fa il punteggio più alto.
+// Creo evento al click
+const click = document.getElementById("button")
+
+click.addEventListener('click',
+    function(){    
+    let userNumber = Math.floor(Math.random() * 6) + 1;
+    let computerNumber = Math.floor(Math.random() * 6) + 1;
+    console.log(userNumber);
+    console.log(computerNumber);
+    // manipolazione dom
+    const userNumberBox = document.querySelector(".user-number");
+    const pcNumberBox = document.querySelector(".pc-number");
+    userNumberBox.innerHTML = userNumber
+    pcNumberBox.innerHTML = computerNumber
+    const result = document.querySelector(".result")
+    // creare la condizione che permette di vincere a chi ha fatto il numero pìu alto 
+    if( userNumber > computerNumber){
+        result.innerHTML = "L'utente è il vincitore"
+    }else if(userNumber === computerNumber){
+        result.innerHTML = "Pareggio!"
+    }else{
+        result.innerHTML = "Il computer ha vinto"
+    }
+
+});
+
 
 // creare una variabile per il giocatore e una per il computer
-let userNumber = Math.floor(Math.random() * 6) + 1;
-let computerNumber = Math.floor(Math.random() * 6) + 1;
 
-console.log(userNumber);
-console.log(computerNumber);
-
-
-// manipolazione dom
-const userNumberBox = document.querySelector(".user-number");
-const pcNumberBox = document.querySelector(".pc-number");
-
-userNumberBox.innerHTML = userNumber
-pcNumberBox.innerHTML = computerNumber
-
-const result = document.querySelector(".result")
-
-// creare la condizione che permette di vincere a chi ha fatto il numero pìu alto 
-if( userNumber > computerNumber){
-    result.innerHTML = "L'utente è il vincitore"
-    // alert(`"Hai vinto!"${userNumber}"vs"${computerNumber}`)
-}else if(userNumber === computerNumber){
-    result.innerHTML = "Pareggio!"
-    // alert(`"Pareggio!!"${userNumber}"vs"${computerNumber}`)
-}else{
-    result.innerHTML = "Il computer ha vinto"
-    // alert(`"Hai perso!"${computerNumber}"vs"${userNumber}`)
-}
