b8147dd315bc11d3b69a18869fa1c5d8e1a20a33
TrvMarco <trevisanello.marco@gmail.com>
2022-05-03 14:47:50 +0200

    aggiunto stuttura html e programma che stampa in console email inserita

---
+++ mail/js/script.js
@@ -0,0 +1,9 @@
+// Chiedi all’utente la sua email
+const userMail = prompt("Inserisci la tua mail");
+// stampa la mail in console 
+console.log('La mail inserita è:',userMail);
+// creo una lista di chi può accedere
+const userMailAccess = ["Filippo","Mattia","Giovanni","Luca"];
+// controlla che sia nella lista di chi può accedere
+
+// stampa un messaggio appropriato sull’esito del controllo.
